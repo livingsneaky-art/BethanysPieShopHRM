5625f3d219d0635036c5b5f0b14bf6d7ad8708d1
livingsneaky-art <ryanllamido@gmail.com>
2023-03-02 21:58:10 +0800

    Initial commit

---
+++ BethanysPieShopHRM/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Here are some test around working with valid identifiers in C#
+ * We can write here as much as we want, this is all commnet
+ */
+
+//Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+//Console.WriteLine("Please enter your name: ");
+
+////The following will accept the name
+//string name = Console.ReadLine();
+
+//string name2 = Console.ReadLine();
+
+//string name_2 = Console.ReadLine();
+
+//string Name_2 = Console.ReadLine();
+
+/*
+not allowed because it start with digit
+string 2Name = Console.ReadLine();
+
+CTRL + K + C = comment all
+CTRL + K + U = uncomment
+*/
+
+/*
+//working with primitive types 
+int monthlywage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isactive = true;
+
+double rating = 99.25;
+
+//byte numberofemployees = 300;//byte can only contain 0 - 255
+
+int hoursworked;
+
+hoursworked = 125;
+
+hoursworked = 148;
+
+//monthlywage = true;//it flag as warning, this is typesafety once the data type is declared it cant not change it's type
+*/
+
+/*
+//Using constant values
+
+const double interestrate = 0.07;
+
+interestrate = 0.08; //can't be change because using a constant value
+*/
+
+/*
+//Creating Strings
+string firstName = "Bethany";
+string lastName = "Smith";
+
+string emptyString = "";
+
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
+*/
+
+
+
+
+
